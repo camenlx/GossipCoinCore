86b4a93a3b4b53e058133f819021f2a342c06944
GOSSIP <dev@gossipcoin.net>
2018-12-04 19:48:28 +0100

    Revert "Update bitcoingui.cpp"
    This reverts commit 27338a9e9ec3be3442c58574cc1f75bdf1f90ba0.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -116,7 +116,7 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle* networkStyle, QWidget* parent) : QMai
 
     GUIUtil::restoreWindowGeometry("nWindow", QSize(850, 550), this);
 
-    QString windowTitle = tr("GOSSIP Coin Core") + " - ";
+    QString windowTitle = tr("Gossipcoin Core") + " - ";
 #ifdef ENABLE_WALLET
     /* if compiled with wallet support, -disablewallet can still disable the wallet */
     enableWallet = !GetBoolArg("-disablewallet", false);
